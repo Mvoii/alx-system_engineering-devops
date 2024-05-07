7a398d3d20180ecbbe2efce3739487aa30706b10
Franklin Mvoi <mvoifranklin@gmail.com>
2024-05-08 00:33:26 +0300

    4 && 100

--- 0x06-regular_expressions/100-textme.rb
+++ 0x06-regular_expressions/100-textme.rb
@@ -1,4 +1,7 @@
 #!/usr/bin/env ruby
 # reex
 
-puts ARGV[0].scan(/\[from:(\+?\w*)\]\s\[to:(\+?\w*)\]\s\[flags:(\S*)\]/).join(',')
+from = ARGV[0].scan(/from:(.*?)\]/)
+to = ARGV[0].scan(/to:(.*?)\]/)
+flags = ARGV[0].scan(/flags:(.*?)\]/)
+puts [from, to, flags].join(',')
--- 0x06-regular_expressions/4-repetition_token_3.rb
+++ 0x06-regular_expressions/4-repetition_token_3.rb
@@ -1,2 +1,4 @@
 #!/usr/bin//env ruby
+# regex
+
 puts ARGV[0].scan(/hbt*n/).join
